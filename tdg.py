8c7ee896a6b9632df7284ef2417dea07db1327d2
Marcus Bowman <miliarch.mb@gmail.com>
2017-01-31 20:26:43 -0800

    Consolidated file/dir writing to a single function (write_paths()), modified module imports a bit

--- tdg.py
+++ tdg.py
@@ -13,9 +13,9 @@ Python Version: 3.5.2
 OS Support: Linux, OS X
 License: MIT
 '''
-import argparse
 import json
 import sys
+from argparse import ArgumentParser
 from pathlib import Path, PurePath
 
 EXEC_DIR = Path.cwd().resolve()
@@ -48,7 +48,7 @@ def parse_args(args):
     """
 
     # Found this bit posted by unutbu here: http://stackoverflow.com/a/4042861
-    class Parser(argparse.ArgumentParser):
+    class Parser(ArgumentParser):
         def error(self, message):
             sys.stderr.write('error: {0}\n'.format(message))
             self.print_help()
@@ -78,37 +78,30 @@ def select_template(template_name):
     return template
 
 
-def create_sub_dirs(sub_dirs):
-    for sub_dir in sub_dirs:
-        Path(sub_dir).mkdir()
-        print(path_add_str(sub_dir))
-
-
-def create_files(files):
-    for file in files:
-        Path(file).touch()
-        print(path_add_str(file))
-
-
 def path_add_str(full_path):
     return '+ {}'.format(full_path)
 
 
 def update_paths(dir_name, template):
-    """ Update all paths in template to absolute values """
-    # Update base_dir
+    """ Update all paths in template dict to absolute values """
     try:
-        template['base_dir'] = PurePath(template['base_dir'], dir_name)
+        template['base_dir'] = [PurePath(template['base_dir'], dir_name)]
     except KeyError:
-        template['base_dir'] = PurePath(EXEC_DIR, dir_name)
+        template['base_dir'] = [PurePath(EXEC_DIR, dir_name)]
 
     for i,sub_dir in enumerate(template['sub_dirs']):
-        template['sub_dirs'][i] = PurePath(template['base_dir'], sub_dir)
+        template['sub_dirs'][i] = PurePath(template['base_dir'][0], sub_dir)
 
     for i,file in enumerate(template['files']):
-        template['files'][i] = PurePath(template['base_dir'], file)
+        template['files'][i] = PurePath(template['base_dir'][0], file)
 
     return template
+
+
+def write_paths(paths, is_file=False):
+    for p in paths:
+        Path(p).touch() if is_file else Path(p).mkdir()
+        print(path_add_str(p))
 
 
 def main(args):
@@ -116,9 +109,9 @@ def main(args):
     template = update_paths(args.dir_name, select_template(args.template))
 
     # Get to business
-    Path(template['base_dir']).mkdir()
-    create_sub_dirs(template['sub_dirs'])
-    create_files(template['files'])
+    write_paths(template['base_dir'], is_file=False)
+    write_paths(template['sub_dirs'], is_file=False)
+    write_paths(template['files'], is_file=True)
 
 
 if __name__ == '__main__':
